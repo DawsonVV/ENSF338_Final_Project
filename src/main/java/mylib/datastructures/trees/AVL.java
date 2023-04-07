49a24e8a7409ba3d04e43d0fa319f02c5fa4b0ba
ParsaKargari <parsakargari@ucalgary.ca>
2023-04-07 16:34:12 -0600

    UPTO Insert

--- src/main/java/mylib/datastructures/trees/AVL.java
+++ src/main/java/mylib/datastructures/trees/AVL.java
@@ -3,68 +3,166 @@ package main.java.mylib.datastructures.trees;
 import main.java.mylib.datastructures.nodes.TNode;
 
 public class AVL extends BST {
+    private TNode root;
     
-    // Member variable
-    TNode root; // references the root of the tree
-    
-    // Constructors
-    // Default constructor initializing root to null
     public AVL() {
         root = null;
     }
-    
-    // Overload constructor AVL(int val) which takes in an integer value,
-    // and creates a TNode and use it as root
+
     public AVL(int val) {
-        root = new TNode(val);
+        this.root = new TNode(val);
     }
-    
-    // Overload constructor AVL(TNode obj) which takes a TNode as an argument
-    // and use it as the root of the tree.
+
     public AVL(TNode obj) {
         root = obj;
-        
-        // If the TNode obj has children, the constructor needs to create a
-        // balanced tree from passed tree by one of the two following options:
-        // • iteratively inserting nodes from the original tree and balancing the
-        // new created AVL tree
-        // • implementing a full tree balancing algorithm (Bonus)
+        root.setParent(null);
+        balanceTree(root);
     }
     
-    // Setter and getter for root: the setter function must check if the node has
-    // children. If children are found it must do the same as the overload constructor.
-    // Hint: it is better to have a helper function (private function) that creates
-    // an the AVL tree and call it for the constructor and the setter
+    private void balanceTree(TNode node) {
+        if (node == null) {
+            return;
+        }
     
-    // Insert(int val): creates a new node with data val to be inserted into the tree
-    // Must maintain the tree balance. It can call the super.insert (insert
-    // function from BST), but will need to also balance the tree after
-    public void Insert(int val) {
-        // TODO: Implement Insert function
+        int balance = calculateBalance(node);
+        node.setBalance(balance);
+    
+        if (balance > 1) {
+            if (calculateBalance(node.getLeft()) > 0) {
+                node = rightRotate(node);
+            } else {
+                node.setLeft(leftRotate(node.getLeft()));
+                node = rightRotate(node);
+            }
+        } else if (balance < -1) {
+            if (calculateBalance(node.getRight()) < 0) {
+                node = leftRotate(node);
+            } else {
+                node.setRight(rightRotate(node.getRight()));
+                node = leftRotate(node);
+            }
+        }
+    
+        if (node.getParent() != null) {
+            balanceTree(node.getParent());
+        } else {
+            root = node;
+        }
     }
     
-    // Insert(TNode node) : inserts the node passed as argument into the tree
-    // Must maintain the tree balance. It can call the super.insert (insert
-    // function from BST), but will need to also balance the tree after
-    public void Insert(TNode node) {
-        // TODO: Implement Insert function
+    private int calculateBalance(TNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return getHeight(node.getLeft()) - getHeight(node.getRight());
     }
     
-    // Delete(int val): finds the node with val as data and deletes it, if not found prints
-    // a statement that the value is not in the tree (Bonus)
-    public void Delete(int val) {
-        // TODO: Implement Delete function (Bonus)
+    private int getHeight(TNode node) {
+        if (node == null) {
+            return -1;
+        }
+        return 1 + Math.max(getHeight(node.getLeft()), getHeight(node.getRight()));
     }
     
-    // Inherited functions
-    // TNode Search(int val): inherited from parent
-    // printInOrder(): inherited from parent
-    // printBF(): inherited from parent
-    
-    // Helper function to balance the tree
-    private void balance() {
-        // TODO: Implement balance function
+    private TNode rightRotate(TNode node) {
+        TNode newRoot = node.getLeft();
+        node.setLeft(newRoot.getRight());
+        if (newRoot.getRight() != null) {
+            newRoot.getRight().setParent(node);
+        }
+        newRoot.setRight(node);
+        newRoot.setParent(node.getParent());
+        node.setParent(newRoot);
+        node.setBalance(calculateBalance(node));
+        newRoot.setBalance(calculateBalance(newRoot));
+        return newRoot;
     }
+    
+    private TNode leftRotate(TNode node) {
+        TNode newRoot = node.getRight();
+        node.setRight(newRoot.getLeft());
+        if (newRoot.getLeft() != null) {
+            newRoot.getLeft().setParent(node);
+        }
+        newRoot.setLeft(node);
+        newRoot.setParent(node.getParent());
+        node.setParent(newRoot);
+        node.setBalance(calculateBalance(node));
+        newRoot.setBalance(calculateBalance(newRoot));
+        return newRoot;
+    }
+    
+    private TNode createAVLTree(TNode node) {
+        if (node == null) {
+            return null;
+        }
+        TNode newRoot = new TNode(node.getData(), node.getBalance(), null, null, null);
+        newRoot.setLeft(createAVLTree(node.getLeft()));
+        newRoot.setRight(createAVLTree(node.getRight()));
+        balanceTree(newRoot);
+        return newRoot;
+    }
+    
+    public void setRoot(TNode node) {
+        root = new TNode(node.getData(), node.getBalance(), null, null, null);
+        if (node.getLeft() != null || node.getRight() != null) {
+            root.setLeft(createAVLTree(node.getLeft()));
+            root.setRight(createAVLTree(node.getRight()));
+        }
+        balanceTree(root);
+    }
+
+    public void insert(int val) {
+        TNode newNode = new TNode(val);
+        if (root == null) {
+            root = newNode;
+        } else {
+            TNode curr = root;
+            TNode parent = null;
+            while (curr != null) {
+                parent = curr;
+                if (val < curr.getData()) {
+                    curr = curr.getLeft();
+                } else {
+                    curr = curr.getRight();
+                }
+            }
+            newNode.setParent(parent);
+            if (val < parent.getData()) {
+                parent.setLeft(newNode);
+            } else {
+                parent.setRight(newNode);
+            }
+        }
+        balanceTree(newNode);
+    }
+
+    public void insert(TNode node) {
+        if (root == null) {
+            root = node;
+        } else {
+            TNode curr = root;
+            TNode parent = null;
+            while (curr != null) {
+                parent = curr;
+                if (node.getData() < curr.getData()) {
+                    curr = curr.getLeft();
+                } else {
+                    curr = curr.getRight();
+                }
+            }
+            node.setParent(parent);
+            if (node.getData() < parent.getData()) {
+                parent.setLeft(node);
+            } else {
+                parent.setRight(node);
+            }
+        }
+        balanceTree(node);
+    }
+
+    
+
+    
+
 }
-
-
